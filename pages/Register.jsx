1ab4bf2a9e2a427b21cec1bc07e37c7b027a20a1
Luciano Barberis <barberisluciano58@gmail.com>
2025-01-16 08:51:50 -0300

    validacion de form register

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,39 +1,70 @@
 import React from 'react'
+import Toastify from 'toastify-js'
 import { useState } from 'react'
 
 const Register = () => {
-        const crearUsuario = async (usuario) => {
-            try {
-                const response = await fetch('http://localhost:8080/user/crear', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(usuario),
-                });
-                const data = await response.json();
-                console.log('Usuario creado:', data);
-            } catch (error) {
-                console.error('Error al crear usuario:', error);
-            }
-        };
-    
-        const [dataForm, setDataForm] = useState({
-            "nombre": "",
-            "contrasena": "",
-            "rol": "REDACTOR"
-        })
-    
-        const handleInput = (e) => {
-            setDataForm({ ...dataForm, [e.target.name]: e.target.value })
-        }
-    
-        const handleSubmit = (e) => {
-            e.preventDefault()
-            console.log("Console.log de la informacion del Form")
-            console.log(dataForm)
-            crearUsuario(dataForm)
-        }
 
-    
+    const alert = (text, color) => {
+        Toastify({
+            text: text,
+            duration: 3000,
+            position: "center",
+            gravity: "bottom",
+            offset: {
+                "y": 70
+            },
+            style: {
+                background: color,
+                boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                height: "50px",
+                fontSize: "22px",
+            },
+        }).showToast();
+    }
+
+    const crearUsuario = async (usuario) => {
+        try {
+            const response = await fetch('http://localhost:8080/user/crear', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(usuario),
+            });
+            const data = await response.json();
+            console.log('Usuario creado:', data);
+            alert("Usuario creado correctamente", "#53796c")
+        } catch (error) {
+            console.error('Error al crear usuario:', error);
+        }
+    };
+
+    const [dataForm, setDataForm] = useState({
+        "nombre": "",
+        "contrasena": "",
+        "rol": "REDACTOR",
+        "repass": ""
+    })
+
+    const handleInput = (e) => {
+        setDataForm({ ...dataForm, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (dataForm.nombre === "" || dataForm.contrasena === "" || dataForm.repass === "") {
+            alert("Rellena todos los campos", "#bb1a1a")
+        }else {
+            if (dataForm.contrasena !== dataForm.repass) {
+                alert("Las contraseñas no son iguales", "#bb1a1a")
+            }else if(dataForm.contrasena.length < 8){
+                alert("La contraseña debe de tener un minimo de 8 caracteres", "#bb1a1a")
+            }else {
+                crearUsuario(dataForm)
+            }
+        }
+        
+    }
+
+
     return (
         <div className="containerLogin">
             <div className='containerForm'>
