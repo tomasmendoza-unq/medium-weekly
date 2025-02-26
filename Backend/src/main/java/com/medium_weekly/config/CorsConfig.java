127843b7ed344c1c293df2ae061d5bde366d550a
Tomas Mendoza <tm1453766@gmail.com>
2025-02-26 11:56:06 -0300

    config envs

--- Backend/src/main/java/com/medium_weekly/config/CorsConfig.java
+++ Backend/src/main/java/com/medium_weekly/config/CorsConfig.java
@@ -6,18 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
+public  class CorsConfig{
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permitir todas las rutas
-                        .allowedOrigins("http://localhost:5174") // Dominio permitido
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
-                        .allowedHeaders("*") // Permitir todos los encabezados
-                        .allowCredentials(true); // Permitir cookies o credenciales
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
     }
